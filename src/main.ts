614253841ffea49524535ce0a40965c663e6ceec
Ahmet Ildirim <ahmetildirim@gmail.com>
2025-02-20 21:42:59 +0100

    Add status bar icon for Inscribe plugin

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from 'obsidian';
+import { Notice, Plugin, setIcon } from 'obsidian';
 import { inlineSuggestions } from "./extension";
 import { Settings, DEFAULT_SETTINGS } from './settings/settings';
 import InscribeSettingsTab from './settings/settings-tab';
@@ -13,6 +13,11 @@ export default class Inscribe extends Plugin {
 		await this.loadProviderManager();
 		await this.setupExtention();
 		this.addSettingTab(new InscribeSettingsTab(this.app, this));
+
+		// Add status bar item
+		const statusBarItemEl = this.addStatusBarItem();
+		const iconEl = statusBarItemEl.createEl('span', { cls: 'inscribe-status' });
+		setIcon(iconEl, 'feather');
 	}
 
 	async loadProviderManager() {
